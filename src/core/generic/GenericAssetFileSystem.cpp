bce6da065d24424c0f3a5cf0e3168b2ced9dd3aa
morten <morten@outracks.no>
2013-02-11 16:19:30 +0100

    -

--- src/core/generic/GenericAssetFileSystem.cpp
+++ src/core/generic/GenericAssetFileSystem.cpp
@@ -10,22 +10,6 @@ namespace Xli
 {
 	FileSystem* CreateAssetFileSystem()
 	{
-#ifdef WIN32
-
-		WCHAR buf[MAX_PATH];
-		DWORD result = GetModuleFileNameW(GetModuleHandle(0), buf, MAX_PATH);
-
-		if (result > 0 && result < MAX_PATH)
-		{
-			String exe = Unicode::Utf16To8(buf, (int)result);
-			String dir = Path::GetDirectoryName(exe);
-
-			if (Disk->IsDirectory(dir))
-				return Disk->CreateSubFileSystem(dir + "/data");
-		}
-
-#endif
-
-		return Disk->CreateSubFileSystem(Disk->GetCurrentDirectory() + "/data");
+		return Disk->CreateSubFileSystem("data");
 	}
 }
