aa3555604db9692b3b7d483a41d863b9c752fe66
Morten <morten@outracks.no>
2013-06-05 16:44:19 +0100

    adding renamed script

---
+++ install_dependencies-OSX2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo port install cmake
+sudo port install mercurial
+sudo port install portaudio +universal
+
+if [ -d src/core/3rdparty/SDL2 ]; then
+    cd src/core/3rdparty/SDL2
+    hg pull && hg update
+    cd -
+else
+    cd src/core/3rdparty
+    hg clone http://hg.libsdl.org/SDL SDL2
+    cd -
+fi
+
+xcodebuild -project 'src/core/3rdparty/SDL2/Xcode/SDL/SDL.xcodeproj' -configuration 'Release' -target 'Static Library' CONFIGURATION_BUILD_DIR='../../lib'
