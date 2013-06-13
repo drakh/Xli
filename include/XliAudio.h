256b3dd2a1518f1d646889c9768a20bb0dbead7d
kristianhasselknippe <kristian_hasselknippe@outlook.com>
2013-06-13 14:53:51 +0200

    Added Xli::SampleReader

--- include/XliAudio.h
+++ include/XliAudio.h
@@ -7,6 +7,7 @@
 
 #include <XliAudio/AudioBuffer.h>
 #include <XliAudio/AudioStream.h>
+#include <XliAudio/SampleReader.h>
 #include <XliAudio/Wav.h>
 
 #endif
---
+++ include/XliAudio/SampleReader.h
@@ -0,0 +1,31 @@
+#ifndef __XLI_AUDIO_SAMPLE_READER_H__
+#define __XLI_AUDIO_SAMPLE_READER_H__
+
+#include <Xli/Stream.h>
+#include "AudioStream.h"
+
+namespace Xli
+{
+	// TODO: Support more input sample formats
+
+	/**
+		\ingroup Audio
+	*/
+	class SampleReader
+	{
+		AudioStream* stream;
+		bool forceStereo;
+		int internalBufferSize;
+		Int16* internalBuffer;
+
+		static float convertSample(Int16 sample);
+
+	public:
+		SampleReader(AudioStream* stream, bool forceStereo);
+		~SampleReader();
+
+		int Read(float* buffer, int expectedSamples);
+	};
+}
+
+#endif
