51ab678921f4edbbe92574a1118bcf826c3bb91a
Morten <morten@outracks.no>
2013-07-31 02:33:16 +0200

    tweaks

--- build-iOS.sh
+++ build-iOS.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
-cd "`dirname "$0"`"
-
-cd projects/iOS
-
+cd "`dirname "$0"`/projects/iOS"
 xcodebuild -alltargets -configuration Debug -sdk iphoneos
 xcodebuild -alltargets -configuration Release -sdk iphoneos
 xcodebuild -alltargets -configuration Debug -sdk iphonesimulator
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+cd "`dirname "$0"`"
 
 if which apt-get; then
 
@@ -10,11 +11,12 @@ elif which port; then
 	port install cmake mercurial portaudio +universal
 
 else
-
-	echo "FATAL ERROR: Unsupported system, patches welcome :)"
+	echo "FATAL ERROR: Unsupported system" >&2
 	exit 1
-
 fi
+
+
+# Download and build SDL2
 
 if [ -d src/core/3rdparty/SDL2/.hg ]; then
     cd src/core/3rdparty/SDL2
@@ -26,18 +28,17 @@ else
 	cd SDL2
 fi
 
-mkdir -p build
-cd build
+if [ `uname` = "Darwin" ]; then
 
-if [ -f /proc/cpuinfo ]; then
-	CPU_COUNT=`grep processor /proc/cpuinfo | wc -l`
-elif [ `uname` = "Darwin" ]; then
-	CPU_COUNT=`sysctl hw.ncpu | cut -d " " -f 2`
+	xcodebuild -project 'Xcode/SDL/SDL.xcodeproj' -configuration 'Release' -target 'Static Library' CONFIGURATION_BUILD_DIR='../../lib'
+
 else
-	CPU_COUNT=1
+
+	mkdir -p build
+	cd build
+
+	../configure --srcdir=.. --prefix=/usr
+	make -j `grep processor /proc/cpuinfo | wc -l`
+	make install
+
 fi
-
-../configure --srcdir=.. --prefix=/usr
-make -j $CPU_COUNT
-make install
-
