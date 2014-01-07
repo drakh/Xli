f71d67eb42f33c8049e3bf7ec832f2171c986ca0
Morten <morten@outracks.no>
2014-01-07 10:56:00 +0100

    android makefile clean up

---
+++ projects/android/jni/XliGL.mk
@@ -0,0 +1,15 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE    := XliGL
+
+LOCAL_SRC_FILES := \
+	../../../src/gl/egl/EGLContext.cpp \
+	
+LOCAL_C_INCLUDES := \
+	../../include \
+
+LOCAL_CFLAGS   := -fexceptions -fno-rtti -fPIC -DPIC -O3
+LOCAL_LDLIBS   := -lEGL -lGLESv2 -landroid
+
+LOCAL_SHARED_LIBRARIES := Xli
+
+include $(BUILD_SHARED_LIBRARY)
