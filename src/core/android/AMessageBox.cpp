154fa7a1e8471f5405db9b762be791660426c2f3
morten <morten@outracks.no>
2014-03-18 12:39:22 +0100

    Android: Also write messagebox to log

--- src/core/android/AMessageBox.cpp
+++ src/core/android/AMessageBox.cpp
@@ -6,6 +6,9 @@ namespace Xli
 {
     DialogResult MessageBox::Show(Window* parent, const String& message, const String& caption, DialogButtons buttons, int hints)
     {
+        // Also print to log
+        PrintLine("MessageBox [" + caption + "]: " + message);
+
         PlatformSpecific::AJniHelper jni;
         int result = PlatformSpecific::AShim::ShowMessageBox(message, caption, (int)buttons, (int)hints);
         return (DialogResult)result;
