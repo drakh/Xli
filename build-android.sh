cd220b73445cc50c78cba02c82d249b11e90a2fb
morten <morten@outracks.no>
2014-04-09 13:48:20 +0200

    adding back old android build code for use on windows (for now)

--- build-android.sh
+++ build-android.sh
@@ -24,6 +24,56 @@ shim)
     ;;
 esac
 
+if [ -f /proc/cpuinfo ]; then
+    JOB_COUNT=`grep processor /proc/cpuinfo | wc -l`
+elif [ "`uname`" = "Darwin" ]; then
+    JOB_COUNT=`sysctl hw.ncpu | cut -d " " -f 2`
+elif [ -n "$NUMBER_OF_PROCESSORS" ]; then
+    JOB_COUNT=$NUMBER_OF_PROCESSORS
+else
+    JOB_COUNT=1
+fi
+
+if [ "$OSTYPE" = "msys" ]; then
+
+SOURCE="projects/android"
+TARGET="lib/android"
+
+cd "$SOURCE"
+
+if [ "$OSTYPE" = "msys" ]; then
+    if [ "$DEBUG" = "1" ]; then
+        cmd "/c call ndk-build -j $JOB_COUNT APP_OPTIM=debug"
+    else
+        cmd "/c call ndk-build -j $JOB_COUNT"
+    fi
+else
+    ndk-build -j $JOB_COUNT "$@"
+fi
+
+cd "$OLDPWD"
+
+if which rsync > /dev/null 2>&1; then
+    CP_CMD="rsync -vru"
+else
+    CP_CMD="cp -vru"
+fi
+
+for arch in "armeabi" "armeabi-v7a" "x86"; do
+    if [ -d "$SOURCE/obj/local/$arch" ]; then
+        mkdir -p "$TARGET/$arch"
+
+        # Note: Copy from 'obj' folder instead of 'libs' if DEBUG -- Non-stripped .so-s there
+        if [ "$DEBUG" = "1" ]; then
+            $CP_CMD "$SOURCE/obj/local/$arch/"*.a "$SOURCE/obj/local/$arch/"*.so "$TARGET/$arch"
+        else
+            $CP_CMD "$SOURCE/obj/local/$arch/"*.a "$SOURCE/libs/$arch/"*.so "$TARGET/$arch"
+        fi
+    fi
+done
+
+else
+
 XLI_DIR="`pwd -P`"
 NDK_FILENAME=`which ndk-build`
 NDK_DIR=`dirname "$NDK_FILENAME"`
@@ -47,16 +97,12 @@ set -- "$@" -DANDROID_ABI="$NDK_ABI"
 set -- "$@" -DANDROID_NATIVE_API_LEVEL="android-9"
 set -- "$@" -DANDROID_STL="stlport_static"
 
-cmake "$@" "$XLI_DIR"
-
-if [ -f /proc/cpuinfo ]; then
-    JOB_COUNT=`grep processor /proc/cpuinfo | wc -l`
-elif [ "`uname`" = "Darwin" ]; then
-    JOB_COUNT=`sysctl hw.ncpu | cut -d " " -f 2`
-elif [ -n "$NUMBER_OF_PROCESSORS" ]; then
-    JOB_COUNT=$NUMBER_OF_PROCESSORS
-else
-    JOB_COUNT=1
+if [ "$OSTYPE" = "msys" ]; then
+    set -- "$@" -G"MinGW Makefiles"
+    MAKE="$NDK_DIR/prebuilt/windows-x86_64/bin/make.exe"
 fi
 
+cmake "$@" "$XLI_DIR"
 make -j $JOB_COUNT
+
+fi
