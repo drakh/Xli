f8352421e9dcaf414ddb5b6c3602352f9dedb734
morten <morten@outracks.no>
2014-07-22 02:23:04 -0700

    adding Display::GetDpi() dummy for linux

--- src/XliPlatform/sdl2/SDL2Display.cpp
+++ src/XliPlatform/sdl2/SDL2Display.cpp
@@ -14,4 +14,18 @@ namespace Xli
         SDL_GetDisplayBounds(index, &rect);
         return Recti(Vector2i(rect.x, rect.y), Vector2i(rect.w, rect.h));
     }
+
+#if !defined(XLI_PLATFORM_OSX) && !defined(XLI_PLATFORM_IOS)
+    float Display::GetDensity(int displayIndex)
+    {
+        // TODO
+        return 1.0f;
+    }
+
+    Vector2 Display::GetDpi(int displayIndex)
+    {
+        // TODO
+        return Vector2(72, 72);
+    }
+#endif
 }
